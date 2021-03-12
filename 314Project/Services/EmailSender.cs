5dc684dab41ff58f4c470fe9670e6fcd7b668160
Raven Chen <RavenChen13581@hotmail.com>
2021-03-12 17:26:08 +0000

    Removed using AspNetCore; in EmailSender Model Somehow causing build errors and not showing in the error list. somehow show it once in a build failure

--- 314Project/Services/EmailSender.cs
+++ 314Project/Services/EmailSender.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Options;
-using AspNetCore;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 
